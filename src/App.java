56499bf936ca452c2b6b9d49bbf07090532af781
gabrielcecconello <gabrielcecconello@hotmail.com>
2024-02-27 16:20:32 -0300

    Primeiro Commit (não sei o que estou fazendo)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Matriz matriz = new Matriz(3, false);
+        matriz.criarAresta();
     }
 }
