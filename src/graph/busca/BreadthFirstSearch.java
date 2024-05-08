d621ed5fd30070b62c6a2012eb4865d85e4501c6
paulatalim <paulactalim@gmail.com>
2024-05-08 17:17:22 -0300

    refinamento de algoritimo

--- src/graph/busca/BreadthFirstSearch.java
+++ src/graph/busca/BreadthFirstSearch.java
@@ -13,9 +13,6 @@ public class BreadthFirstSearch {
     private int[][] graphM;
     private ArrayList<Character> vertices;
 
-    // int vertices;
-    // LinkedList<Integer>[] adjList;
-
     public BreadthFirstSearch(List<No> grafo) {
         graphL = grafo;
     }
@@ -32,46 +29,83 @@ public class BreadthFirstSearch {
         ArrayList<Character> tree = new ArrayList<>();
         boolean allNodeVisited = false;
 
+        int[] L = new int[vertices.size()];
+        int[] nivel = new int[vertices.size()];
+        char[] pai = new char[vertices.size()];
+        int time = 0;
+
+        // Inicializa as variaveis
+        for (int i = 0; i < vertices.size(); i++) {
+            L[i] = 0;
+            nivel[i] = 0;
+            pai[i] = '-';
+        }
+
         // Mark the current node as visited and enqueue it
-        visited[vertices.indexOf(verticeInicial)] = true;
-        queue.add(vertices.indexOf(verticeInicial));
+        // visited[vertices.indexOf(verticeInicial)] = true;
+        // queue.add(vertices.indexOf(verticeInicial));
 
-        while(allNodeVisited) {
-
-            // Iterate over the queue
-            while (!queue.isEmpty()) {
-                // Dequeue a vertex from queue and print it
-                int currentNode = queue.poll();
-                tree.add(vertices.get(currentNode));
-                //System.out.print(currentNode + " ");
-                
-                // Get all adjacent vertices of the dequeued
-                // vertex currentNode If an adjacent has not
-                // been visited, then mark it visited and
-                // enqueue 
-                
-                for (int i = 0; i < vertices.size(); i++) {
-                    if(graphM[currentNode][i] != 0 || graphM[i][currentNode] != 0) {
-                        if(!visited[i]) {
-                            visited[i] = true;
-                            queue.add(i);
-                        }
-                    }
-                }
-            }
+        while(!allNodeVisited) {
 
             // Verifica se todos os vertices foram visitados
             allNodeVisited = true;
             for(int i = 0; i < visited.length; i ++) {
-                if(!visited[i]) {
-                    visited[i] = true;
-                    queue.add(i);
+                if(L[i] == 0) {
                     allNodeVisited = false;
-                    tree.add('-');
+                    queue.add(i);
+                    time++;
+                    L[i] = time;
+                    break;
                 }
             }
+
+            // Realiza a busca em largura
+            while (!queue.isEmpty()) {
+                // Dequeue a vertex from queue and print it
+                int currentNode = queue.poll();
+                
+                
+                // Anlise dos visinhos
+                for (int neighbor = 0; neighbor < vertices.size(); neighbor++) {
+                    // Busca do vertice vizinho
+                    if(graphM[currentNode][neighbor] != 0) {
+                        if(L[neighbor] == 0) {
+                            // Aresta pai
+                            pai[neighbor] = vertices.get(currentNode);
+                            nivel[neighbor] = nivel[currentNode] + 1;
+                            time++;
+                            L[neighbor] = time;
+                            queue.add(neighbor);
+                            tree.add(vertices.get(currentNode));
+                        }
+                        // else if(nivel[neighbor] == nivel[currentNode] + 1) {
+                        //     // Aresta tio
+                        // } else if(
+                        //         nivel[neighbor] == nivel[currentNode] && 
+                        //         pai[neighbor] == pai[currentNode] && 
+                        //         L[neighbor] > L[currentNode]) 
+                        //     {
+                        //     // Aresta irmao
+                        // } else if(
+                        //         nivel[neighbor] == nivel[currentNode] && 
+                        //         pai[neighbor] != pai[currentNode] && 
+                        //         L[neighbor] > L[currentNode]) 
+                        //     {
+                        //     // Aresta primo
+                        // }
+
+                        // if(!visited[i]) {
+                        //     visited[i] = true;
+                        //     queue.add(i);
+                        // }
+                    }
+                }
+            }
+
+            
         }
 
+        exibirBusca(L, nivel, pai);
 
         return tree;
     }
@@ -83,51 +117,123 @@ public class BreadthFirstSearch {
         boolean allNodeVisited = false;
         ArrayList<Character> tree = new ArrayList<>();
 
-        // Mark the current node as visited and enqueue 
-        for(int i = 0; i < graphL.size(); i++) {
-            if(graphL.get(i).getId() == verticeInicial) {
-                visited[i] = true;
-                queue.add(i);
-                break;
-            }
+        int[] L = new int[graphL.size()];
+        int[] nivel = new int[graphL.size()];
+        char[] pai = new char[graphL.size()];
+        int time = 0;
+
+        // Inicializa as variaveis
+        for (int i = 0; i < graphL.size(); i++) {
+            L[i] = 0;
+            nivel[i] = 0;
+            pai[i] = '-';
         }
 
+        // Mark the current node as visited and enqueue 
+        // for(int i = 0; i < graphL.size(); i++) {
+        //     if(graphL.get(i).getId() == verticeInicial) {
+        //         visited[i] = true;
+        //         queue.add(i);
+        //         break;
+        //     }
+        // }
+
         while (!allNodeVisited) {
+            // Verifica se ha vertice nao visitado
+            allNodeVisited = true;
+            for(int i = 0; i < graphL.size(); i++) {
+                if(L[i] == 0) {
+                    time ++;
+                    L[i] = time;
+                    queue.add(i);
+                    allNodeVisited = false;
+                    break;
+                }
+            }
+
+            // for(int i = 0; i < visited.length; i++) {
+            //     if(!visited[i]) {
+            //         allNodeVisited = false;
+            //         queue.add(i);
+            //         visited[i] = true;
+            //         tree.add('-');
+            //     }
+            // }
             
             // Iterate over the queue
             while (!queue.isEmpty()) {
                 // Dequeue a vertex from queue and print it
                 int currentNode = queue.poll();
-                tree.add(graphL.get(currentNode).getId());
+                // tree.add(graphL.get(currentNode).getId());
                 
                 // Verifica se os vizinhos de cada vertice ja foram visitados
                 // Se nao foram, sao marcados como visitas e eh adiconado a fila
                 for(int i = 0; i < graphL.get(currentNode).qnt_aresta(); i++) {
-                    for(int j = 0; j < graphL.size(); j++) {
-                        if(graphL.get(j).getId() == graphL.get(currentNode).getAresta(i)) {
-                            if (!visited[j]) {
-                                visited[j] = true;
-                                queue.add(j);
-                                break;
+                    for(int neighbor = 0; neighbor < graphL.size(); neighbor++) {
+                        if(graphL.get(neighbor).getId() == graphL.get(currentNode).getAresta(i)) {
+
+                            if(L[neighbor] == 0) {
+                                // Aresta pai
+                                pai[neighbor] = graphL.get(currentNode).getId();
+                                nivel[neighbor] = nivel[currentNode] + 1;
+                                time++;
+                                L[neighbor] = time;
+                                queue.add(neighbor);
+                                tree.add(graphL.get(currentNode).getId());
                             }
+                            // else if(nivel[neighbor] == nivel[currentNode] + 1) {
+                            //     // Aresta tio
+                            // } else if(
+                            //         nivel[neighbor] == nivel[currentNode] && 
+                            //         pai[neighbor] == pai[currentNode] && 
+                            //         L[neighbor] > L[currentNode]) 
+                            //     {
+                            //     // Aresta irmao
+                            // } else if(
+                            //         nivel[neighbor] == nivel[currentNode] && 
+                            //         pai[neighbor] != pai[currentNode] && 
+                            //         L[neighbor] > L[currentNode]) 
+                            //     {
+                            //     // Aresta primo
+                            // }
+
+                            break;
+
+                            // if (!visited[j]) {
+                            //     visited[j] = true;
+                            //     queue.add(j);
+                            //     break;
+                            // }
                         }
                     }
                 }
             }
-
-            // Verifica se ha vertice nao visitado
-            allNodeVisited = true;
-            for(int i = 0; i < visited.length; i++) {
-                if(!visited[i]) {
-                    allNodeVisited = false;
-                    queue.add(i);
-                    visited[i] = true;
-                    tree.add('-');
-                }
-            }
         }
 
+        
+        exibirBusca(L, nivel, pai);
+
         return tree;
+    }
+
+    private void exibirBusca(int[] L, int[] nivel, char[] pai) {
+        System.out.print("L ");
+
+        for(int i = 0; i < L.length; i++) {
+            System.out.print(L[i] + " ");
+        }
+
+        System.out.print("\nnivel ");
+
+        for(int i = 0; i < nivel.length; i++) {
+            System.out.print(nivel[i] + " ");
+        }
+
+        System.out.print("\npai ");
+
+        for(int i = 0; i < pai.length; i++) {
+            System.out.print(pai[i] + " ");
+        }
     }
 
     // Function to perform Breadth First Search on a graph
