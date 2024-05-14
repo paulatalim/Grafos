51583ce171841b477fd6b146df0c7c74e7ad3462
paulatalim <paulactalim@gmail.com>
2024-05-14 14:44:04 -0300

    documentacao de metodos

--- src/graph/caminho/Dijkstra.java
+++ src/graph/caminho/Dijkstra.java
@@ -24,6 +24,10 @@ public class Dijkstra {
         this.isPonderado = isPonderado;
     }
 
+    /**
+     * Verifica se o grafo possui peso negativo
+     * @return true (se haver peso negativo) ou false (se nao haver peso negativo)
+     */
     private boolean containPesoNegativo() {
         if(graphL == null) {
             if(isPonderado) {
@@ -51,6 +55,11 @@ public class Dijkstra {
         return false;
     }
 
+    /**
+     * Verifica se um vertice existe na lista de adjacencia
+     * @param vertice - char (vertice a ser verificado)
+     * @return true (se o vertice estiver no grafo) ou false (se o vertice ser invalido)
+     */
     private boolean validarVerticeLista(char vertice) {
         for(int i = 0; i < graphL.size(); i++) {
             if(graphL.get(i).getId() == vertice) {
@@ -61,6 +70,11 @@ public class Dijkstra {
         return false;
     }
 
+    /**
+     * Verifica se um vertice existe na matriz de adjacencia
+     * @param vertice - char (vertice a ser verificado)
+     * @return true (se o vertice estiver no grafo) ou false (se o vertice ser invalido)
+     */
     private boolean validarVerticeMatriz(char vertice) {
         for(int i = 0; i < vertices.size(); i++) {
             if(vertices.get(i) == vertice) {
@@ -71,6 +85,12 @@ public class Dijkstra {
         return false;
     }
 
+    /**
+     * Calcula o caminho minimo entre a raiz e a saida na matriz de adjaencia
+     * @param raiz vertice em que o algoritimo ira iniciar
+     * @param saida vertice em que sera calculado o caminho
+     * @return Integer (tamanho do caminho minimo da raiz ate a saida), ou null (caso ocorra um erro) ou Integer.MAX_VALUE (caso nao exista caminho entre os vertices)
+     */
     private Integer calcularCaminhoMinimoMatriz(char raiz, char saida) {
         Integer[] distance = new Integer[vertices.size()];
         Character[] predecessores = new Character[vertices.size()];
@@ -150,6 +170,12 @@ public class Dijkstra {
         return null;
     }
 
+    /**
+     * Calcula o caminho minimo entre a raiz e a saida na lista de adjaencia
+     * @param raiz vertice em que o algoritimo ira iniciar
+     * @param saida vertice em que sera calculado o caminho
+     * @return Integer (tamanho do caminho minimo da raiz ate a saida), ou null (caso ocorra um erro) ou Integer.MAX_VALUE (caso nao exista caminho entre os vertices)
+     */
     private Integer calcularCaminhoMinimoLista(char raiz, char saida) {
         Integer[] distance = new Integer[graphL.size()];
         Character[] predecessores = new Character[graphL.size()];
@@ -228,15 +254,23 @@ public class Dijkstra {
         return null;
     }
 
+    /**
+     * Calcula o caminho minimo entre a os vertices
+     * @param vertice1 vertice a ser analisado
+     * @param vertice2 vertice a ser analisado
+     * @return Integer (tamanho do caminho minimo da raiz ate a saida), ou null (caso ocorra um erro) ou Integer.MAX_VALUE (caso nao exista caminho entre os vertices)
+     */
     public Integer calcularCaminhoMinimo(char vertice1, char vertice2) {
         // Verifica se ha peso negativo no grafo
         if(containPesoNegativo()) return null;
 
+        // vaerifica o caminho minimo com a matriz
         if(graphL == null) {
             if(!validarVerticeMatriz(vertice1)) return null;
             return calcularCaminhoMinimoMatriz(vertice1, vertice2);
         }
 
+        // vaerifica o caminho minimo com a lista
         if(!validarVerticeLista(vertice1)) return null;
         return calcularCaminhoMinimoLista(vertice1, vertice2);
     }
